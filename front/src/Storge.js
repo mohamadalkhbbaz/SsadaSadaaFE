da5a94d0ad5b846fe9d1253fc6d1d50d9e9c0235
mohamed <mohamed>
2023-10-30 16:51:32 +0200

    inital

---
+++ front/src/Storge.js
@@ -0,0 +1,36 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>Hi</h1>
+// </body>
+// <script>
+//     console.log('hi');
+//     localStorage.setItem('itemId',10)
+//     localStorage.setItem('id','113212312')
+//     localStorage.setItem('id','newValue')
+    
+//     // localStorage.getItem('itemId');
+//     // localStorage.removeItem('itemId')
+//     // localStorage.clear();
+    
+//     console.log(localStorage.key(0)); //==> itemId
+//     console.log(localStorage.getItem('itemId').length); //==> 2
+//     console.log(localStorage.key(0).length); // get length for "itemId" ==> 6
+
+
+//     // sessionStorage ==> like LocalStorage 
+//     sessionStorage.setItem('itemId',10)
+//     sessionStorage.setItem('id','113212312')
+
+//     document.cookie = 'sessionId=vopazbeqizwvhs4kxfeiic4b,Path=/; HttpOnly; Expires=Thu,'+ new Date(2023,1,1).toUTCString() + ';Secret'
+
+//     document.cookie = 'ssada=test'    
+
+//     // sessionId=vopazbeqizwvhs4kxfeiic4b; Path=/; HttpOnly; Expires=Thu, 18 Jul 2024 11:30:36 GMT;Secret   
+//     </script>
+// </html>
