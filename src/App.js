7e5053c5884ed3bbcc290ff38b5a27c3ad80ccaa
mohamed <mohamed>
2023-10-31 16:06:09 +0200

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+function App() {
+  return (
+    <div className="App">
+     <Container>
+      <Row className='justify-content-center'>
+        <div className='rectangle'>
+          <Col>Our their growth and success.</Col>
+        </div>
+      </Row>
+    </Container>
+    </div>
+  );
+}
+
+export default App;
