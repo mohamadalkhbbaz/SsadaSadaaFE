dc230d9bc6c2f8ef6a7fe1889d559213e291ee83
mohamed <mohamed>
2023-11-01 14:28:38 +0200

    inital

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import VarEx from './VarEx';
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,7 @@ function App() {
         <div className='rectangle'>
           <Col>Our their growth and success.</Col>
         </div>
+        <VarEx />
       </Row>
     </Container>
     </div>
--- src/varEx.js
+++ src/varEx.js
@@ -3,20 +3,46 @@
 // let : block scope  > reChange value 
 // const = block scope > !reChange value 
 
-
 import React from 'react'
-var var1 = "test";
 
+const ss = 'ss';
 const fun1 = _ => {
-  console.log(var1);
+     var1 = "test";
+     var var1;
+console.log(var1);
+
 }
 fun1();
-cd
 
-const varEx = () => {
+
+// 2. Arrow Function 
+// const sayGo =  _  => c.l("go");
+// const say=  _  =>  2;
+// const sayMyName =  (name)  => c.l(name);
+
+// 3. Export and Import
+// //fileName.js
+// export defult func;
+
+// //mainApp.jc
+// import func from './fileName.js'
+
+// if export multi func 
+// export func1
+// export func2 
+// or 
+// export { func1, func2 }
+// >> import {func1} from'./fileName.js'
+
+// u can export func or variable
+// u can export func or variable Or Image
+
+
+
+const VarEx = () => {
   return (
     <div>varEx</div>
   )
 }
 
-export default varEx
+export default VarEx
