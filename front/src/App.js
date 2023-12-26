f4015a443b760b9861c565a14dec6597ad62fc9c
mohamed <mohamadalkhbbaz@gmail.com>
2023-12-26 11:19:56 +0300

    inital

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Data } from "./Data/Data";
 
 function App() {
+
+  const [data,setData] = useState([]);
+
+  useEffect(() => {
+    setData(Data)
+  },[])
+
+  const handleDeleteData = () => {
+    setData([]);
+  }
+  const handleShowData = () => {
+    setData(Data);
+  }
+  
+  const lengthData = data.length;
+  const dataIsEmpty = data.length == 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-center my-1">
+          <Col sm="8">لديك اليوم{lengthData} مواعيد </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col sm="8">
+            <div className="rectangle p-2">
+              {  dataIsEmpty ? <h4 className="text-center">لا يوجد مواعيد اليوم</h4>: 
+              data.map((item) => {
+                return (
+                  <div className="d-flex p-1">
+                    <img className="pic m-2" src={"./"+item.img} />
+                    <div>
+                      <h5 className="d-inline">{item.name}</h5>
+                      <p className="fs-6">{item.reminderData}</p>
+                    </div>
+                  </div>
+                );
+              })
+              }
+            </div>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col className="d-flex justify-content-between my-2 " sm="8">
+            <button className="btn-style" onClick={handleDeleteData}> مسح البيانات </button>
+            <button className="btn-style" onClick={handleShowData} > عرض البيانات </button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ front/src/Data/Data.jsx
@@ -0,0 +1,7 @@
+export const Data = [
+    { id: 1, name: "محمد رامي", reminderData: "2 مساءُ", img: "img-1.png" },
+    { id: 2, name: "احمد رامز", reminderData: "12 صباحاُ", img: "img-1.png" },
+    { id: 3, name: "عبود عن الوعود",reminderData: "6 مساءُ",img: "img-1.png" },
+    { id: 4, name: "سامر سومر", reminderData: "6 مساءُ", img: "img-1.png" },
+    { id: 5, name: "رفعت الرافي", reminderData: "4 مساءُ", img: "img-1.png" }
+  ];
